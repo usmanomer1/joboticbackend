9d9b9b3838698fb261bc69205c1287a24a07e4ed
uomer <uumer58@yahoo.com>
2025-08-08 00:43:34 +0500

    fix(convex): correct cron to use function reference with args for purgeOldData

--- convex/crons.ts
+++ convex/crons.ts
@@ -4,14 +4,10 @@ import { api } from "./_generated/api";
 const crons = cronJobs();
 
 // Daily cleanup of old sessions and jobs
-crons.interval(
-  "purge-old-convex-data",
-  { hours: 24 },
-  async (ctx) => {
-    // Default retention: 7 days
-    await ctx.runMutation(api.jobs.purgeOldData, { maxAgeDays: 7 });
-  }
-);
+crons.interval("purge-old-convex-data", { hours: 24 }, {
+  handler: api.jobs.purgeOldData,
+  args: { maxAgeDays: 7 },
+});
 
 export default crons;
 
