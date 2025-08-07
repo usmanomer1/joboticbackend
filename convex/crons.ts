87d073488b340b2f0ea3c1b23a05832e6f186bda
uomer <uumer58@yahoo.com>
2025-08-08 00:46:00 +0500

    fix(convex): use internal.* function reference for cron scheduling to satisfy functionReference requirement

--- convex/crons.ts
+++ convex/crons.ts
@@ -1,11 +1,11 @@
 import { cronJobs } from "convex/server";
-import { api } from "./_generated/api";
+import { internal } from "./_generated/api";
 
 const crons = cronJobs();
 
 // Daily cleanup of old sessions and jobs (use no-arg mutation for cron)
 crons.interval("purge-old-convex-data", { hours: 24 }, {
-  handler: api.jobs.purgeOldDataCron,
+  handler: internal.jobs.purgeOldDataCron,
 });
 
 export default crons;
