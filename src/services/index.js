1a67dbbdaf85fbd0648095c6a54c68725c080406
uomer <uumer58@yahoo.com>
2025-08-10 03:39:48 +0500

    fix: export services properly from index.js

--- src/services/index.js
+++ src/services/index.js
@@ -1,3 +1,8 @@
 // Service Index - Re-export all services for better discoverability
 
 // Export all service modules
+module.exports = {
+  jobSearchService: require('./jobSearch.service'),
+  aiMatchingService: require('./aiMatching.service'),
+  usageTrackingService: require('./usageTracking.service')
+};
